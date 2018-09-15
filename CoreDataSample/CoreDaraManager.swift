b73f5f9d9c874ba572285937c4044504c3473ddd
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-09-15 09:51:08 +0900

    不必要なコメントの削除

--- CoreDataSample/CoreDaraManager.swift
+++ CoreDataSample/CoreDaraManager.swift
@@ -51,32 +51,8 @@ class CoreDaraManager<T:NSManagedObject>: NSObject {
             self.fetchRequest.fetchBatchSize = getCount!
         }
         
-        /*
-         someDataBプロパティが100のレコードを指定している
-         let predicate = NSPredicate(format: "%K = %d", "someDataB", 100)
-         */
         self.fetchRequest.predicate = NSPredicate(format: "\(key) == %@", serch as! CVarArg )
         
-        /*
-        複数
-         let predicate = NSPredicate(format: "id = %d AND name = %@", 1, "tanaka")
-         
-         配列で
-         let predicate = NSCompoundPredicate(type:.AndPredicateType, subpredicates: [
-         NSPredicate(format: "id = %d", 1),
-         NSPredicate(format: "name = %@", "tanaka")
-         ])
-         
-         */
-        
-        /*
-         sort いつか使う時のために
-         let sortSample = NSSortDescriptor(key: "title", ascending:true)
-         self.fetchRequest.sortDescriptors = [sortSample]
-         
-         */
-        
-
         do {
             self.fetchRequests = try viewContext.fetch(self.fetchRequest)
             latestmessage = "成功"
